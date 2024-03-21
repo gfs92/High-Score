e7e0e6d192a050642040e82dd0b4ef70d1b4e25a
Grant Skinner <gfs00200@gmail.com>
2024-03-21 17:00:15 -0500

    api url change

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:3001/api/v1/games";
+export const API_URL = "https://high-score-api.onrender.com/api/v1/games";
 export const BG_IMG =
   "https://cdn.pixabay.com/photo/2019/05/29/16/00/retro-4237850_1280.jpg";
